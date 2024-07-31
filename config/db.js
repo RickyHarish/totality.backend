87f05f44997643ed1bd4b2654c7aa6c0d3f666dc
RickyHarish <rickyharish30@gmail.com>
2024-07-31 10:43:20 +0530

    db connection update

--- config/db.js
+++ config/db.js
@@ -3,9 +3,14 @@ import orderModel from '../models/oderModel.js'
 import userModel from '../models/userModel.js'
 import productModel from '../models/productModel.js'
 
+const username = encodeURIComponent("Harish");
+const password = encodeURIComponent("abcd1234");
+
+const URL = `mongodb+srv://${username}:${password}@cluster0.20bemwo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0` || process.env.MONGO_URI
+
 const connectDB = async()=>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(URL)
         console.log(`MongoDB Connected: ${ conn.connection.host}`.cyan.underline)
         await orderModel.createIndexes()
         await userModel.createIndexes()
